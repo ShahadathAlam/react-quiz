477e708eb59e507bf0251ba9709780bc638295a0
ShahadathAlam <shahadathalam@ymail.com>
2024-08-23 12:20:23 +0600

    starting a new quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 const initialState = {
   questions: [],
   // 'loading','error', 'ready','active','finished'
@@ -15,6 +16,9 @@ const reducer = function (state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+
     default:
       throw new Error("Action Unknown");
   }
@@ -37,7 +41,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
