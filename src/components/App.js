6fc5ae3845580aee293bc927b99455c2e0c474cb
ShahadathAlam <shahadathalam@ymail.com>
2024-08-26 07:19:57 +0600

    Restart Quiz

--- src/components/App.js
+++ src/components/App.js
@@ -45,6 +45,17 @@ const reducer = function (state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+
+    // return {
+    //   ...state,
+    //   index: 0,
+    //   answer: null,
+    //   points: 0,
+    //   highscore: 0,
+    //   status: "ready",
+    // };
     default:
       throw new Error("Action Unknown");
   }
@@ -104,6 +115,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
